0fc91181cf8486df2ca3ecced6f15ea074ed038d
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-07 20:33:38 +0900

    feat: 구매할 상품명과 수량 입력 기능 추가

---
+++ src/view/InputView.js
@@ -0,0 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+import InputHandler from '../controller/InputHandler.js';
+
+class InputView {
+  #inputHandler;
+
+  constructor() {
+    this.#inputHandler = new InputHandler();
+  }
+
+  async inputOrder() {
+    const input = await Console.readLineAsync(
+      '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n'
+    );
+  }
+}
+
+export default InputView;
