55ec0c400f59cf00453b0f499a09a499c43d2fd7
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-07 22:21:39 +0900

    refactor: 주문 객체 생성 기능 분리

--- src/controller/InputHandler.js
+++ src/controller/InputHandler.js
@@ -1,18 +1,20 @@
 class InputHandler {
   convertInputToSystemOrder(input) {
     const orderItems = input.split(', ');
-
-    const order = orderItems.map((orderItem) => {
-      const [item, quantity] = orderItem.slice(1, -1).split('-');
-      return { item: item, quantity: Number(quantity) };
-    });
-
+    const order = this.#transformToOrderObject(orderItems);
     return order;
   }
 
   convertInputToBoolean(input) {
     if (input == 'Y') return true;
     return false;
+  }
+
+  #transformToOrderObject(orderItems) {
+    return orderItems.map((orderItem) => {
+      const [item, quantity] = orderItem.slice(1, -1).split('-');
+      return { item: item, quantity: Number(quantity) };
+    });
   }
 }
 
