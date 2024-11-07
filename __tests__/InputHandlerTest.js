db3b69e871a8315bc19d3d100a5e5423d0398b94
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-07 21:55:56 +0900

    test: 입력된 상품명과 수량을 시스템에서 사용할 수 있는 형식으로 변환하는 기능 테스트 추가

---
+++ __tests__/InputHandlerTest.js
@@ -0,0 +1,28 @@
+import InputHandler from '../src/controller/InputHandler.js';
+
+describe('InputHandler 클래스 테스트', () => {
+  let inputHandler;
+
+  beforeEach(() => {
+    inputHandler = new InputHandler();
+  });
+
+  test.each([
+    [
+      '[콜라-3], [에너지바-5], [사이다-2]',
+      [
+        { item: '콜라', quantity: 3 },
+        { item: '에너지바', quantity: 5 },
+        { item: '사이다', quantity: 2 },
+      ],
+    ],
+    ['[콜라-3]', [{ item: '콜라', quantity: 3 }]],
+  ])(
+    '입력된 상품명과 수량을 시스템에서 사용할 수 있는 형식으로 변환한다.',
+    (input, expectedOrderItems) => {
+      expect(inputHandler.convertInputToSystemOrder(input)).toEqual(
+        expectedOrderItems
+      );
+    }
+  );
+});
