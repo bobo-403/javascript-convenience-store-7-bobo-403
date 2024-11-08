c9ad3401fcb3a46cf4a0e0a6792154c3c5ac17a9
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-08 19:08:05 +0900

    test: [상품명-수량] 형식에 맞지 않은 주문이면 예외가 발생하는 기능 테스트 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -12,4 +12,20 @@ describe('Validation 클래스 테스트', () => {
       validation.validateNotEmpty('');
     }).toThrow(ERROR_MESSAGE.EMPTY_INPUT);
   });
+
+  test.each(['[콜라]', '콜라-10', '[콜라-]', '[-1]'])(
+    '[상품명-수량] 형식에 맞지 않은 주문이면 예외가 발생한다.',
+    (order) => {
+      expect(() => {
+        validation.validateOrderFormat(order);
+      }).toThrow(ERROR_MESSAGE.INVALIDATE_ORDER_FORAMT);
+    }
+  );
+
+  test('[상품명-수량] 형식에 맞는 주문이면 예외가 발생하지 않는다.', () => {
+    const order = '[콜라-2]';
+    expect(() => {
+      validation.validateOrderFormat(order);
+    }).not.toThrow();
+  });
 });
