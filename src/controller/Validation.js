c98f331d1c9b70c4ca9c1498b81c4b7be1100e20
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-08 19:19:07 +0900

    feat: 존재하지 않는 상품을 입력한 경우 예외가 발생하는 기능 추가

--- src/controller/Validation.js
+++ src/controller/Validation.js
@@ -1,6 +1,12 @@
 import { ERROR_MESSAGE } from '../constant/Message.js';
 
 class Validation {
+  #inventory;
+
+  constructor(inventory) {
+    this.#inventory = inventory;
+  }
+
   validateNotEmpty(input) {
     if (input === '') {
       throw new Error(ERROR_MESSAGE.EMPTY_INPUT);
@@ -10,6 +16,12 @@ class Validation {
   validateOrderFormat(order) {
     if (!/^\[([^\]]+)-(.+)\]$/.test(order))
       throw new Error(ERROR_MESSAGE.INVALIDATE_ORDER_FORAMT);
+  }
+
+  validateProductName(name) {
+    const productsName = this.#inventory.getProductsName();
+    if (!productsName.includes(name))
+      throw new Error(ERROR_MESSAGE.INVALIDATE_PRODUCT_NAME);
   }
 }
 
