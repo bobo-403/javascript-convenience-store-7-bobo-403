96a8e71a97196b7249ad70221c9e6378393c53aa
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-08 10:37:55 +0900

    refactor: 주문 입력 메시지 상수로 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import InputHandler from '../controller/InputHandler.js';
+import Message from '../constant/Message.js';
 
 class InputView {
   #inputHandler;
@@ -9,9 +10,7 @@ class InputView {
   }
 
   async inputOrder() {
-    const input = await Console.readLineAsync(
-      '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n'
-    );
+    const input = await Console.readLineAsync(Message.ENTER_PRODUCT_QUANTITY);
   }
 
   async inputYesOrNo(question) {
