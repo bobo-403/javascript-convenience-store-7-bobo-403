b62bdd1c458d1418bd0a9a30340ab326f84e6993
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-08 18:20:07 +0900

    chore: 메러메시지 내용 수정

--- src/constant/Message.js
+++ src/constant/Message.js
@@ -14,10 +14,13 @@ export const PROGRESS_MESSAGE = Object.freeze({
 
 export const ERROR_MESSAGE = Object.freeze({
   EMPTY_INPUT: '[ERROR] 입력값이 없습니다.',
-  INVALIDATE_ORDER_FORAMT: '[상품-수량] 형식으로 입력해주세요.',
-  INVALIDATE_PRODUCT_NAME: '존재하지 않는 상품명입니다.',
-  INVALIDATE_PRODUCT_QUANTITY: '수량은 0이상 정수로 입력해주세요',
-  NON_NUMERIC_INPUT: '수량은 숫자로 입력해주세요.',
-  INSUFFICIENT_STOCK: '입력한 수량이 재고보다 많습니다.',
-  INVALID_DECISION_INPUT: 'Y 또는 N로 입력해주세요.',
+  INVALIDATE_ORDER_FORAMT:
+    '[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.',
+  INVALIDATE_PRODUCT_NAME:
+    '[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.',
+  INVALIDATE_PRODUCT_QUANTITY: '[ERROR] 수량은 0이상 정수로 입력해주세요',
+  NON_NUMERIC_INPUT: '[ERROR] 수량은 숫자로 입력해주세요.',
+  INSUFFICIENT_STOCK:
+    '[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.',
+  INVALID_DECISION_INPUT: '[ERROR] Y 또는 N로 입력해주세요.',
 });
