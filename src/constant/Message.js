ca449fcf54aec8945c15b57fcc72b7292459218e
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-08 14:10:33 +0900

    feat: 편의점 환영 인사 출력 기능 추가

--- src/constant/Message.js
+++ src/constant/Message.js
@@ -1,5 +1,5 @@
 const PROGRESS_MESSAGE = Object.freeze({
-  GRETTING_MESSAGE: '안녕하세요. W편의점입니다.\n',
+  GRETTING_MESSAGE: '안녕하세요. W편의점입니다.',
   AVAILABLE_PRODUCTS: '현재 보유하고 있는 상품입니다.\n',
   ENTER_PRODUCT_QUANTITY:
     '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n',
---
+++ src/view/OutputView.js
@@ -0,0 +1,8 @@
+import { Console } from '@woowacourse/mission-utils';
+import PROGRESS_MESSAGE from '../constant/Message.js';
+
+class Ouputput {
+  showGreeting() {
+    Console.print(PROGRESS_MESSAGE.GRETTING_MESSAGE);
+  }
+}
