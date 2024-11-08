178aa85d2df3b74023ea1129c15f46aa42943eef
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-08 19:18:13 +0900

    test: 존재하지 않는 상품을 입력한 경우 예외가 발생하는 기능 테스트 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,10 +1,13 @@
 import { ERROR_MESSAGE } from '../src/constant/Message.js';
 import Validation from '../src/controller/Validation.js';
+import Inventory from '../src/model/Inventory.js';
 
 describe('Validation 클래스 테스트', () => {
   let validation;
+  let inventory;
   beforeEach(() => {
-    validation = new Validation();
+    inventory = new Inventory();
+    validation = new Validation(inventory);
   });
 
   test('입력값이 비어있으면 예외가 발생한다.', () => {
@@ -28,4 +31,22 @@ describe('Validation 클래스 테스트', () => {
       validation.validateOrderFormat(order);
     }).not.toThrow();
   });
+
+  test.each(['아이스티', '삼각김밥', '불닭볶음면'])(
+    '존재하지 않는 상품을 입력한 경우 예외가 발생한다.',
+    (name) => {
+      expect(() => {
+        validation.validateProductName(name);
+      }).toThrow(ERROR_MESSAGE.INVALIDATE_PRODUCT_NAME);
+    }
+  );
+
+  test.each(['콜라', '사이다', '초코바'])(
+    '존재하는 상품을 입력한 경우 예외가 발생하지 않는다.',
+    (name) => {
+      expect(() => {
+        validation.validateProductName(name);
+      }).not.toThrow();
+    }
+  );
 });
