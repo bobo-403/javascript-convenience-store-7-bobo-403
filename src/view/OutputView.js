e0e4372e3da678d252396f674b981eedce55d46b
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-09 09:56:06 +0900

    feat: 현재 상품 재고현황을 출력하는 기능 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,8 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
-import PROGRESS_MESSAGE from '../constant/Message.js';
+import { PROGRESS_MESSAGE } from '../constant/Message.js';
+import Inventory from '../model/Inventory.js';
 
-class Ouputput {
+class OutputView {
   showGreeting() {
     Console.print(PROGRESS_MESSAGE.GRETTING_MESSAGE);
   }
+
+  showInventory() {
+    const inventory = new Inventory();
+    Console.print(PROGRESS_MESSAGE.AVAILABLE_PRODUCTS);
+    Console.print(inventory.getProductsString());
+  }
 }
+
+export default OutputView;
