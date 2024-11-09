9ac97559b126b06ece0336dfebb71b428888b4c4
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-09 09:03:54 +0900

    feat: InputHandler 클래스에 입력 값 검증 로직 추가

--- src/controller/InputHandler.js
+++ src/controller/InputHandler.js
@@ -1,19 +1,34 @@
+import Validation from '../controller/Validation.js';
 class InputHandler {
+  #validation;
+
+  constructor() {
+    this.#validation = new Validation();
+  }
+
   convertInputToSystemOrder(input) {
+    this.#validation.validateNotEmpty(input);
     const orderItems = input.split(',');
-    const orders = orderItems.map((orderItem) => {
-      return this.#generateOrderObject(orderItem);
+    const orders = orderItems.map((order) => {
+      return this.#generateOrderObject(order);
     });
     return orders;
   }
 
   convertInputToBoolean(input) {
+    this.#validation.validateNotEmpty(input);
+    this.#validation.validateDecisionInput(input);
     if (input == 'Y') return true;
     return false;
   }
 
-  #generateOrderObject(orderItem) {
-    const [item, quantity] = orderItem.slice(1, -1).split('-');
+  #generateOrderObject(order) {
+    this.#validation.validateOrderFormat(order);
+    const [item, quantity] = order.slice(1, -1).split('-');
+    this.#validation.validateProductName(item);
+    this.#validation.validateIsNumber(quantity);
+    this.#validation.validatePositiveInteger(quantity);
+    this.#validation.validateStockAvailability(name, quantity);
     return { item: item, quantity: Number(quantity) };
   }
 }
