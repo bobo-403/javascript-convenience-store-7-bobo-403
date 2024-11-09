e6e1bcfd22e63bfede79fd15fc062ba07ed44e0c
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-09 17:08:55 +0900

    test: 오늘 날짜가 프로모션 기간 내에 포함되는지 확인하는 기능 테스트 추가

---
+++ __tests__/PromotionManagerTest.js
@@ -0,0 +1,32 @@
+import PromotionManager from '../src/controller/PromotionManager.js';
+
+describe('PromotionManager 클래스 테스트', () => {
+  let promotionManager;
+  beforeEach(() => {
+    promotionManager = new PromotionManager();
+  });
+
+  test('현재 해당 프로모션의 기간이면 true를 반환한다.', () => {
+    const promotion = {
+      name: '탄산2+1',
+      buy: 2,
+      get: 1,
+      startDate: new Date('2023-12-31T15:00:00.000Z'),
+      endDateNextDay: new Date('2024-12-31T15:00:00.000Z'),
+    };
+
+    expect(promotionManager.IsPromotionPeriod(promotion)).toBe(true);
+  });
+
+  test('현재 해당 프로모션의 기간이 아니면 false를 반환한다.', () => {
+    const promotion = {
+      name: '탄산2+1',
+      buy: 2,
+      get: 1,
+      startDate: new Date('2023-12-31T15:00:00.000Z'),
+      endDateNextDay: new Date('2024-04-31T15:00:00.000Z'),
+    };
+
+    expect(promotionManager.IsPromotionPeriod(promotion)).toBe(false);
+  });
+});
