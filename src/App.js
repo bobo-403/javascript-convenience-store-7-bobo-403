627cdfebaac7864725adcd64ba5e5767f45257aa
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-11 00:56:09 +0900

    feat: 주문 처리 및 결제 진행 기능 추가
    - 재고 현황을 출력하고 주문을 받는다.
    - 결제를 진행한다.
    - 영수증을 출력한다.
    - 재방문 여부에 따라 다시 진행한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { PROGRESS_MESSAGE } from './constant/Message.js';
+import retry from './controller/Retry.js';
+import StoreManager from './controller/StoreManager.js';
+import InputView from './view/InputView.js';
+import OutputView from './view/OutputView.js';
+
 class App {
-  async run() {}
+  #inputView;
+  #outputView;
+  #storeManager;
+  constructor() {
+    this.#inputView = new InputView();
+    this.#outputView = new OutputView();
+    this.#storeManager = new StoreManager();
+  }
+  async run() {
+    let reVisit = false;
+    do {
+      this.#outputView.showGreeting();
+      this.#outputView.showInventory();
+      const orders = await retry(() => this.#inputView.inputOrder());
+      const receipt = await this.#storeManager.processPayment(orders);
+      this.#outputView.showReceipt(receipt);
+      reVisit = await retry(() =>
+        this.#inputView.inputYesOrNo(PROGRESS_MESSAGE.ASK_ADDITIONAL_PURCHASE)
+      );
+    } while (reVisit);
+  }
 }
 
 export default App;
