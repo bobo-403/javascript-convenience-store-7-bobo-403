056a8c32b7df719528379bde9f1157f333a759c4
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-10 23:51:56 +0900

    feat: 프로모션 상품 개수에 비해 부족한 개수를 일반 상품으로 계속 구매할것인지 묻는 기능 추가

--- src/controller/PromotionManager.js
+++ src/controller/PromotionManager.js
@@ -1,17 +1,30 @@
 import { DateTimes } from '@woowacourse/mission-utils';
 import FileManager from './FileManager.js';
+import { PROGRESS_MESSAGE } from '../constant/Message.js';
+import InputView from '../view/InputView.js';
+import retry from './Retry.js';
 
 class PromotionManager {
   #promotions;
+  #inputView;
 
   constructor() {
     const fileManager = new FileManager();
     this.#promotions = fileManager.readPromotionsFile();
+    this.#inputView = new InputView();
   }
 
   getPromotionSetSize(product) {
     const promotion = this.getPromotionInfo(product.promotion);
     return promotion.buy + promotion.get;
+  }
+
+  async askContinuePurchase(name, shortQuantity) {
+    return await retry(() =>
+      this.#inputView.inputYesOrNo(
+        PROGRESS_MESSAGE.ASK_PURCHASE_WITHOUT_PROMOTION(name, shortQuantity)
+      )
+    );
   }
 
   getPromotionInfo(promotionName) {
