1d1c6f1fa55245a3e01edd46eda7f1c20ae307ff
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-11 00:26:35 +0900

    feat: 해당 상품명에 대한 프로모션 상품의 재고 정보를 반환하는 기능 추가

--- src/controller/StoreManager.js
+++ src/controller/StoreManager.js
@@ -7,6 +7,11 @@ class StoreManager {
     this.#inventory = new Inventory();
   }
 
+  getPromotoinProduct(name) {
+    const products = this.#inventory.getProducts(name);
+    return products.find((product) => product.promotion !== 'null');
+  }
+
   getBasicProduct(name) {
     const products = this.#inventory.getProducts(name);
     return products.find((product) => product.promotion == 'null');
