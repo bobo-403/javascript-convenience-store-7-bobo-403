54122c3e625dcc0ae0da7546a875e5644ddeb705
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-10 18:08:30 +0900

    refactor: Y/N 입력값에 해 검증 및 boolean 값 변환 로직 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -18,6 +18,7 @@ class InputView {
 
   async inputYesOrNo(question) {
     const input = await Console.readLineAsync(question);
+    return this.#inputHandler.convertInputToBoolean(input);
   }
 }
 
