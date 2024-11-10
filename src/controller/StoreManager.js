274a3773de852e26c960de2dd482ded7ead8cb28
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-11-11 00:28:29 +0900

    feat: 하나의 주문에 대해 주문 세부 정보(상품명, 가격, 프로모션 상품에서의 구매량, 일반 상품에서의 구매량)를 반환하는 기능 추가

--- src/controller/StoreManager.js
+++ src/controller/StoreManager.js
@@ -1,10 +1,42 @@
 import Inventory from '../model/Inventory.js';
+import PromotionManager from './PromotionManager.js';
 
 class StoreManager {
   #inventory;
+  #promotionManager;
 
   constructor() {
     this.#inventory = new Inventory();
+    this.#promotionManager = new PromotionManager();
+  }
+
+  async getOrderDetail(order) {
+    let promotionQuantity = 0;
+    let basicQuantity = 0;
+    let totalQuantity = order.quantity;
+    const promotionProduct = this.getPromotoinProduct(order.name);
+    if (
+      promotionProduct &&
+      this.#promotionManager.isPromotionPeriod(promotionProduct)
+    ) {
+      const {
+        totalQuantity: newTotalQuantity,
+        promotionQuantity: newPromotionQuantity,
+      } = await this.#promotionManager.getTotalAndPromoQuantities(
+        order.quantity,
+        promotionProduct
+      );
+      totalQuantity = newTotalQuantity;
+      promotionQuantity = newPromotionQuantity;
+    }
+    basicQuantity = totalQuantity - promotionQuantity;
+    const basicProduct = this.getBasicProduct(order.name);
+    return {
+      name: order.name,
+      price: basicProduct.price,
+      promotionQuantity,
+      basicQuantity,
+    };
   }
 
   getPromotoinProduct(name) {
